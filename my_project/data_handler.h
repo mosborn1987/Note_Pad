11ba602c2712ec30317121006a44a5163b7c9165
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-01-31 14:03:45 -0500

    Search for string almost working

--- my_project/data_handler.h
+++ my_project/data_handler.h
@@ -3,7 +3,8 @@
 
 #include <file_handler.h>
 
-char test_string[256] = "WR";
+
+char temp_data_holder[256];
 
 long char_position = 5;
 long fp_position = 0;
@@ -11,6 +12,12 @@ long fp_position = 0;
 void read_line(void);
 void read_line(void)
 {
+    transfer_buffer[0] = '\0';
+    // Read in first line
+    fread(transfer_buffer, 1, 39 , fp_ORIGINAL);
+
+    // Print Material
+    printf("Material Read: %s\n", transfer_buffer);
 
 
 
@@ -19,27 +26,104 @@ void read_line(void)
 void find_string();
 void find_string()
 {
-    // Find First Char
-    char_position = strstr(fp_ORIGINAL, test_string);
+    // Read first line of code
+    read_line();
 
-    printf("\nChar_Position = %d", char_position);
+    char test_string[256] = "WR";
+    char temp_data_buffer[50];
+
+    char *p_char;
+    p_char = strstr(transfer_buffer, test_string);
+
+    printf("\n p_char = %c", p_char[0]);
+//    printf("\n p_char = %d", p_char[0]);
+//    printf("\n temp_data_buffer = %c",temp_data_buffer);
+//    printf("\n temp_data_buffer = %d",temp_data_buffer);
+
+    char temp_c = 'a';
+    int temp_i = 0;
+
+//    for(temp_i =)
+
+//    strncpy(temp_data_buffer, p_char, p_char+strlen(test_string));
+
+//    printf("\n Did it work? %s",temp_data_buffer);
 
 
 
-    // Rewind the fp position
-    rewind(fp_ORIGINAL);
+//
+//
+//    int main ()
+//{
+//  char str[] ="This is a simple string";
+//  char * pch;
+//  pch = strstr (str,"simple");
+//  strncpy (pch,"sample",6);
+//  puts (str);
+//  return 0;
+//}
 
-    // Get Current Location
-    fp_position = ftell(fp_ORIGINAL);
+//
+//
+//    printf("Material Read: %s\n", transfer_buffer);
+//
+//    // Locate string in transfer_buffer
+//    char *string_location;
+//    string_location = strstr(transfer_buffer, "test_string");
+//
+//    printf("\nString Location: %d", string_location);
+//
+//    // Calculate String location
+//    string_location = string_location - transfer_buffer;
+//    printf("\nString Offset: %c", string_location);
 
-    // Fseek the location of the current fp
-    fseek(fp_ORIGINAL, (fp_position - char_position), SEEK_SET);
+//    // Beginning of file
+//    fseek(fp_ORIGINAL, string_location, SEEK_SET);
+//
+//    // Read
+//    fread(transfer_buffer, 1, 10, fp_ORIGINAL);
+//
+//    // Print String
+//    printf("\nRead Material: %s", transfer_buffer);
 
-    // Read String
-    fread(transfer_buffer, 1, strlen(test_string), fp_ORIGINAL);
+//
+///* strstr example */
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main ()
+//{
+//  char str[] ="This is a simple string";
+//  char * pch;
+//  pch = strstr (str,"simple");
+//  strncpy (pch,"sample",6);
+//  puts (str);
+//  return 0;
+//}
 
-    // Print Read material
-    printf("\nData Read: %s", transfer_buffer);
+
+
+//    // Find First Char
+//    char_position = strstr(fp_ORIGINAL, test_string);
+//
+//    printf("\nChar_Position = %d", char_position);
+//
+//
+//
+//    // Rewind the fp position
+//    rewind(fp_ORIGINAL);
+//
+//    // Get Current Location
+//    fp_position = ftell(fp_ORIGINAL);
+//
+//    // Fseek the location of the current fp
+//    fseek(fp_ORIGINAL, (fp_position - char_position), SEEK_SET);
+//
+//    // Read String
+//    fread(transfer_buffer, 1, strlen(test_string), fp_ORIGINAL);
+//
+//    // Print Read material
+//    printf("\nData Read: %s", transfer_buffer);
 
 }
 
