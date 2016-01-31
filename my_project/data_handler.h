e602f3c99d62f0762d8668181d4d27dbed9186c9
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-01-31 11:53:20 -0500

    Search for String Not Yet Working. Still able to read a file though :(

--- my_project/data_handler.h
+++ my_project/data_handler.h
@@ -6,6 +6,7 @@
 char test_string[256] = "WR #: ";
 
 long char_position = 0;
+long fp_position = 0;
 
 void find_string();
 void find_string()
@@ -13,11 +14,22 @@ void find_string()
     // Find First Char
     char_position = strstr(fp_ORIGINAL, test_string);
 
+    printf("\nChar_Position = %d", char_position);
+
+    // Rewind the fp position
+    rewind(fp_ORIGINAL);
+
+    // Get Current Location
+    fp_position = ftell(fp_ORIGINAL);
+
+    // Fseek the location of the current fp
+    fseek(fp_ORIGINAL, (fp_position - char_position), SEEK_SET);
+
     // Read String
     fread(transfer_buffer, 1, strlen(test_string), fp_ORIGINAL);
 
     // Print Read material
-    printf("%s", transfer_buffer);
+    printf("\nData Read: %s", transfer_buffer);
 
 
     // Test the following string against a passed value
